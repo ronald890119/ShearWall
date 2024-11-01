5ca66173493f6c7b2543d55195a38a0ac971769e
Zelin Liang <zelin353598223@gmail.com>
2024-11-01 23:41:09 +1100

    Add method to load STDC-Net model in predict.py file

--- predict.py
+++ predict.py
@@ -15,6 +15,7 @@ import numpy as np
 from torchvision.models._utils import IntermediateLayerGetter
 import argparse
 from segmentation.train_ronald import MyClassifier
+from segmentation.stdc import STDCNetForSegmentation
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Optional app description')
@@ -51,6 +52,35 @@ if __name__ == '__main__':
         
         plt.imshow(output, cmap='gray')
         plt.show()
+        
+    elif args.model == 'STDC':
+        transform = transforms.Compose([
+            transforms.Resize((512, 512)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ])
+        # Load the trained model
+        stdc_model = STDCNetForSegmentation(num_classes=1)
+        stdc_model.load_state_dict(torch.load('model_stdc.pth'))
+        stdc_model.eval()  
+        
+        img = Image.open(args.img).convert('RGB')
+        # width, height = img.size
+        img = transform(img).unsqueeze(0)
+        img = img.to(device)
+        
+        output = stdc_model(img)['out']
+        output = torch.sigmoid(output)
+        output = torch.squeeze(output, 0)
+        output = np.transpose(output.cpu().detach().numpy(), (1, 2, 0))
+                
+        output[output < 0.5] = 0
+        output[output >= 0.5] = 1
+        
+        plt.imshow(output, cmap='gray')
+        plt.show()
+        
+        
     else:
         print('No model available!')
         exit()
